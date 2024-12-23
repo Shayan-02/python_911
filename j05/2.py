321333644cfa4286aefcae8fc9123f5ba75a8260
Shayan Shahbazi <upm200@gmail.com>
2024-12-23 15:50:58 +0330

    j06

---
+++ j05/2.py
@@ -0,0 +1,3 @@
+n = int(input("enter a number: "))
+
+print("zoj" if n % 2 == 0 else "fard")
---
+++ j05/3.py
@@ -0,0 +1,8 @@
+a = int(input("enter number1: "))
+b = int(input("enter number2: "))
+c = int(input("enter number3: "))
+
+print(a, b, c, sep="*")
+print(a, end="*")
+print(b, end="*")
+print(c)
